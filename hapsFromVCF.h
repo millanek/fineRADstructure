53286ddae78d12c1f6777c7009969f2433b391a6
millanek <millanek@gmail.com>
2018-03-07 10:05:25 +0100

    need this file too

---
+++ hapsFromVCF.h
@@ -0,0 +1,17 @@
+//
+//  hapsFromVCF.h
+//  fineRADstructure
+//
+//  Created by Milan Malinsky on 05/03/2018.
+//  Copyright © 2018 Milan Malinsky. All rights reserved.
+//
+
+#ifndef hapsFromVCF_h
+#define hapsFromVCF_h
+
+#include <stdio.h>
+
+int VCFhapsMain(int argc, char** argv);
+void parseVCFoptions(int argc, char** argv);
+
+#endif /* hapsFromVCF_h */
